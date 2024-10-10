afa1862f6eb26cfc47ecfb05dc3b5dd149ec0177
Marcelo Pereira <marcelo.pereira.antonio0@gmail.com>
2024-10-10 00:13:25 -0300

    fix: rm route

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,8 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ProductController::class, 'index'])->name('product.index');
 Route::get('/create', [ProductController::class, 'create'])->name('product.create');
 Route::post('/store', [ProductController::class, 'store'])->name('product.store');
-Route::get('/auth', [AuthController::class, 'generateAccessToken']);
